8453bec77ce6347ae9ca6ccd5bb4081ab030f3db
Burak Cihan <burakcihan622@gmail.com>
2023-04-26 00:43:41 +0300

    typescript update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import Card from './components/Card';
-
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Card />
-      <Card />
+      <Card name="Burak Cihan" title="Lorem ipsum bla bla bla" />
+      <Card name="Berat Cihan" title="Lorem ipsum bla bla bla" />
+      <Card name="Fuat Cihan" title="Lorem ipsum bla bla bla" />
     </SafeAreaView>
   );
 };
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import styles from './Card.style';
 
-const Card = () => {
+type TProps = {
+  name: string;
+  title: string;
+};
+
+const Card: React.FC<TProps> = (props: TProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.body}>
-        <Text style={styles.title}>Burak Cihan</Text>
-        <Text style={styles.text}>Lorem ipsum bla bla bla</Text>
+        <Text style={styles.title}>{props.name}</Text>
+        <Text style={styles.text}>{props.title}</Text>
       </View>
       <TouchableOpacity style={styles.button_container}>
         <Text style={styles.button_title}>I Like</Text>
