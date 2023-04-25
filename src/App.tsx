90226e5a29a1d4d64c25f80124f5200e8d2ebbf6
Burak Cihan <burakcihan622@gmail.com>
2023-04-26 02:30:46 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import news_data from './news_data.json';
 import NewsCard from './components/NewsCard';
 
+type TProps = {
+  item: {
+    imageUrl: string;
+    title: string;
+    description: string;
+    author: string;
+    u_id: string | number;
+  };
+};
 const App: React.FC = () => {
+  const renderNews = ({item}: TProps) => {
+    return <NewsCard news={item} />;
+  };
+  const keyExtractor = (item: {u_id: string | number}) => item.u_id.toString();
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <FlatList
           data={news_data}
-          renderItem={({item}) => <NewsCard news={item} />}
+          renderItem={renderNews}
+          keyExtractor={keyExtractor}
         />
       </View>
     </SafeAreaView>
